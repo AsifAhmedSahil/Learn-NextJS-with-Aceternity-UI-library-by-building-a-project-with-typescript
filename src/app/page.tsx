b459bdabd2ea7810a57df88bf90d019ceac2c744
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-10 16:40:57 +0600

    Solving Hero Section challenges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <h1 className="text-center text-2xl mt-8">Next js for next level</h1>
-    </>
+      <HeroSection/>
+    </main>
   );
 }
