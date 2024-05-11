d6364ffb419d32c9427fae6a230f871b5f62e3c5
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-11 16:26:49 +0600

    Grid background and Moving cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import TestimonialCards from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroSection/>
       <FeaturedCourses/>
       <WhyChooseUs />
+      <TestimonialCards/>
       
     </main>
   );
