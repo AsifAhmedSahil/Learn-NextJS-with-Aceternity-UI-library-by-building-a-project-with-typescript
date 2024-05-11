aa183dd3b6061df4f5bda71494ec2e96c773611f
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-11 15:52:21 +0600

    Designing Sticky Scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <main className="min-h-screen w-full bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <h1 className="text-center text-2xl mt-8">Next js for next level</h1>
       <HeroSection/>
       <FeaturedCourses/>
+      <WhyChooseUs />
+      
     </main>
   );
 }
