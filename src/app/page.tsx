a372407dd4915d26ef06e3b429c358fa4c915e29
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-12 23:16:27 +0600

    Card Hover effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebiners from "@/components/UpcomingWebiners";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <FeaturedCourses/>
       <WhyChooseUs />
       <TestimonialCards/>
+      <UpcomingWebiners/>
       
     </main>
   );
