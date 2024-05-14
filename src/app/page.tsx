fbe96f63a308ae8907eb20f80ff31b7925bdb755
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-14 22:11:54 +0600

    contact page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import WhyChooseUs from "@/components/WhyChooseUs";
 export default function Home() {
   return (
     <main className="min-h-screen w-full bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-center text-2xl mt-8">Next js for next level</h1>
+      
       <HeroSection/>
       <FeaturedCourses/>
       <WhyChooseUs />
